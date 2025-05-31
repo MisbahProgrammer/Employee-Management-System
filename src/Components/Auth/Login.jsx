890ce7d37a8964cf0378d5cee54bceb5b6ae2847
MisbahProgrammer <misbahrehman891@gmail.com>
2025-05-31 15:43:04 +0500

    Created Login Page

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './../../App.css'
 function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+   const Submitter = (e) =>{
+      e.preventDefault()
+    console.log('Email is' ,email);
+    console.log('Password is', password);
+    setEmail("")
+    setPassword("")
+    
+    
+   }
   return (
     <div className='flex items-center justify-center w-screen h-screen'>
       <div className='flex items-center justify-center flex-col w-[30%] h-[70%] border'>
         <h2>Login</h2>
-        <form action="" className='flex flex-col'>
-          <input className='py-2 px-4 rounded-2xl border' type="text" placeholder='Email' />
-          <input className='py-2 px-4 rounded-2xl border' type="text" placeholder='Password' />
+        <form action="" className='flex flex-col gap-3' onSubmit={(e) =>{
+          Submitter(e)
+        }}>
+          <input 
+          value={email}
+          onChange={(e)=>{
+            setEmail(e.target.value)
+          }}
+          className='py-3 px-5 rounded-full border border-gray-300 outline-none' 
+          type="text" placeholder='Email' required 
+          />
+          <input
+          value={password} 
+          onChange={(e)=>{
+            setPassword(e.target.value)
+          }}
+          className='py-3 px-5 rounded-full border border-gray-300 outline-none' 
+          type="text" placeholder='Password' required 
+          />
+          <button className='py-3 px-5 rounded-full bg-teal-600 text-white cursor-pointer'>Login</button>
         </form>
       </div>
     </div>
